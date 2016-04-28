f3cd552564ed54d08e30350e909a44b1015fafd2
lv.hang <289751112@qq.com>
2016-04-28 10:20:17 +0800

    init pizza = null

--- src/SimpleFactory/SimplePizzaFactory.java
+++ src/SimpleFactory/SimplePizzaFactory.java
@@ -1,16 +1,14 @@
 package SimpleFactory;
 
 public class SimplePizzaFactory {
-	private static Pizza pizza;
+	private static Pizza pizza = null;
 	
 	public static Pizza create(String type) {
 		if ("NorthPizza".equals(type)) {
 			pizza = new NorthPizza();
-			return pizza;
 		} else if ("SouthPizza".equals(type)){
 			pizza = new SouthPizza();
-			return pizza;
 		}
-		return null;
+		return pizza;
 	}
 }
